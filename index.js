029c69e18e4632b384c03e14e13be21c6f051f63
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-04-01 07:22:20 +0200

    fix: asynchronuous read search value

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sortTab = (tab) => tab.sort((a, b) => a - b);
 
 const tab = [6, 3, 7, 10, -4, 9, 6, 1, 2, 7, 9, 8];
 
-let max = tab.length, min = 1, mediane = (min + max) / 2;
+let max = tab.length, min = 1, mediane = Math.round((min + max) / 2);
 let searchValue;
 
 // read user input searched value
@@ -13,17 +13,24 @@ const readline = require('readline').createInterface({
 });
 
 readline.question('Enter the search value : ', (value) => {
-    searchValue = value;
-    return readline.close();
+    searchValue = parseInt(value);
+    sortTab(tab);
+
+    while (searchValue !== tab[mediane] && min <= max) {
+        if (searchValue < tab[mediane]) {
+            max = mediane - 1;
+        } else {
+            min = mediane + 1;
+        }
+        mediane = Math.round((min + max) / 2);
+    }
+
+    if (searchValue === tab[mediane]) {
+        console.log("Value founded at position ", mediane);
+    } else {
+        console.log("Value does not exist in the array");
+    }
+
+    readline.close();
 });
 
-while (searchValue !== tab[mediane] && min <= max) {
-    if (searchValue < tab[mediane]) {
-        max = mediane - 1;
-    } else {
-        min = mediane + 1;
-    }
-    mediane = (min + max) / 2;
-}
-
-console.log(sortTab(tab));
