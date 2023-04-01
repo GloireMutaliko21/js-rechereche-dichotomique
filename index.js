35ecda0ebe22c86f07754db250d9247d3b04e3e3
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-04-01 06:48:57 +0200

    feat: init read user input value

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const tab = [6, 3, 7, 10, -4, 9, 6, 1, 2, 7, 9, 8];
+
+let max = tab.length, min = 1, mediane = (min + max) / 2;
+let searchValue;
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Entrez une valeur : ', (nom) => {
+    if (typeof nom !== "number") {
+        return console.log("Invalid value")
+    }
+    readline.close();
+});
