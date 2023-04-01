fd4e930a01392645501e8ef0d150e13dff3edb68
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-04-01 07:01:23 +0200

    feat: search min, max and mediane according the searchValue

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// this function sorts a passed array in ascending order
 const sortTab = (tab) => tab.sort((a, b) => a - b);
 
 const tab = [6, 3, 7, 10, -4, 9, 6, 1, 2, 7, 9, 8];
@@ -5,14 +6,24 @@ const tab = [6, 3, 7, 10, -4, 9, 6, 1, 2, 7, 9, 8];
 let max = tab.length, min = 1, mediane = (min + max) / 2;
 let searchValue;
 
+// read user input searched value
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readline.question('Entrez une valeur : ', (nom) => {
-    searchValue = nom;
+readline.question('Enter the search value : ', (value) => {
+    searchValue = value;
     return readline.close();
 });
+
+while (searchValue !== tab[mediane] && min <= max) {
+    if (searchValue < tab[mediane]) {
+        max = mediane - 1;
+    } else {
+        min = mediane + 1;
+    }
+    mediane = (min + max) / 2;
+}
 
 console.log(sortTab(tab));
